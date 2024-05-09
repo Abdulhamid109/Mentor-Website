5cfa83ce12c437bf9a6af26d24d42cb50ee09a56
Abdulhamid Patel <114407664+Abdulhamid109@users.noreply.github.com>
2024-05-09 22:08:30 +0530

    Update ReportGeneration.dart

--- lib/Mentor Panel/ReportGeneration.dart
+++ lib/Mentor Panel/ReportGeneration.dart
@@ -5,64 +5,76 @@ import 'package:google_fonts/google_fonts.dart';
 class ReportGeneration extends StatefulWidget {
   final String batchName;
   final String studentMessage;
-  final int year;
-  final int month; 
-  const ReportGeneration({super.key, required this.batchName, required this.studentMessage,required this.month,required this.year});
+  final int startYear;
+  final int startMonth;
+  final int endYear;
+  final int endMonth;
+
+  const ReportGeneration({
+    Key? key,
+    required this.batchName,
+    required this.studentMessage,
+    required this.startYear,
+    required this.startMonth,
+    required this.endYear,
+    required this.endMonth,
+  }) : super(key: key);
 
   @override
   State<ReportGeneration> createState() => _ReportGenerationState();
 }
 
 class _ReportGenerationState extends State<ReportGeneration> {
-  late DateTime startOfMonth;
-  late DateTime endOfMonth;
-  String ?month;
+  late DateTime startDate;
+  late DateTime endDate;
+  late String startMonth;
+  late String endMonth;
 
   @override
   void initState() {
     super.initState();
-    startOfMonth = DateTime(widget.year, widget.month,1);
-    endOfMonth = DateTime(widget.year,widget.month+1,1).subtract(const Duration(days: 1));
-    month = getMonthfromint(widget.month);
+    startDate = DateTime(widget.startYear, widget.startMonth, 1);
+    endDate = DateTime(widget.endYear, widget.endMonth + 1, 1).subtract(const Duration(days: 1));
+    startMonth = getMonthFromInt(widget.startMonth);
+    endMonth = getMonthFromInt(widget.endMonth);
   }
-  
 
-Future<QuerySnapshot<Map<String, dynamic>>> getChatsForMonth(DateTime startOfMonth, DateTime endOfMonth) {
-  return FirebaseFirestore.instance
-      .collection(widget.studentMessage)
-      .where('messageTimeStamp', isGreaterThanOrEqualTo: Timestamp.fromDate(startOfMonth), isLessThan: Timestamp.fromDate(endOfMonth.add(Duration(days: 1))))
-      .get();
-}
+  Future<QuerySnapshot<Map<String, dynamic>>> getChatsForDateRange(DateTime startDate, DateTime endDate) {
+    return FirebaseFirestore.instance
+        .collection(widget.studentMessage)
+        .where('messageTimeStamp', isGreaterThanOrEqualTo: Timestamp.fromDate(startDate), isLessThan: Timestamp.fromDate(endDate.add(Duration(days: 1))))
+        .get();
+  }
 
-  String getMonthfromint(int integermonth){
-    switch (integermonth) {
+  String getMonthFromInt(int integerMonth) {
+    switch (integerMonth) {
       case 1:
-      return "January";
+        return "January";
       case 2:
-      return "February";
+        return "February";
       case 3:
-      return "March";
+        return "March";
       case 4:
-      return "April";
+        return "April";
       case 5:
-      return "May";
+        return "May";
       case 6:
-      return "June";
+        return "June";
       case 7:
-      return "July";
+        return "July";
       case 8:
-      return "August";
+        return "August";
       case 9:
-      return "September";
+        return "September";
       case 10:
-      return "October";
+        return "October";
       case 11:
-      return "November";
+        return "November";
       case 12:
-      return "December";
-        
+        return "December";
+
       default:
-       return 'Invalid Month';
+        return 'Invalid Month';
     }
   }
 
@@ -83,7 +95,7 @@ Future<QuerySnapshot<Map<String, dynamic>>> getChatsForMonth(DateTime startOfMon
               shadowColor: Colors.white,
               child: Padding(
                 padding: const EdgeInsets.all(5.0),
-                child: Text('$month , ${widget.year}',style: GoogleFonts.habibi(fontSize:16,fontWeight:FontWeight.bold,)),
+                child: Text('$startMonth ${widget.startYear} - $endMonth ${widget.endYear}', style: GoogleFonts.habibi(fontSize: 16, fontWeight: FontWeight.bold)),
               ),
             ),
           ),
@@ -94,45 +106,45 @@ Future<QuerySnapshot<Map<String, dynamic>>> getChatsForMonth(DateTime startOfMon
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: <Widget>[
-              //Fetch data based on month
+              //Fetch data based on date range
               Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: getChatsForMonth(startOfMonth, endOfMonth).asStream(),
+                child: FutureBuilder<QuerySnapshot>(
+                  future: getChatsForDateRange(startDate, endDate),
                   builder: (context, snapshot) {
-                    if(snapshot.connectionState == ConnectionState.waiting){
+                    if (snapshot.connectionState == ConnectionState.waiting) {
                       return Center(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const CircularProgressIndicator(),
-                            SizedBox(height: 4,),
-                            Text('fetching data....',style: GoogleFonts.abel(),)
-                          ],
-                        ),
-                      );
-                    }
-          
-                    if(snapshot.hasError){
-                      return Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.error),
-                            const SizedBox(height: 4,),
-                            Text(snapshot.error.toString(),style: GoogleFonts.abel(),)
+                            CircularProgressIndicator(),
+                            SizedBox(height: 4),
+                            Text('Fetching data...', style: GoogleFonts.abel()),
                           ],
                         ),
                       );
                     }
 
-                    if(snapshot.data == null || snapshot.data!.docs.isEmpty){
+                    if (snapshot.hasError) {
                       return Center(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            const Icon(Icons.error),
-                            const SizedBox(height: 4,),
-                            Text('No Data Found',style: GoogleFonts.abel(),)
+                            Icon(Icons.error),
+                            SizedBox(height: 4),
+                            Text(snapshot.error.toString(), style: GoogleFonts.abel()),
+                          ],
+                        ),
+                      );
+                    }
+
+                    if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.error),
+                            SizedBox(height: 4),
+                            Text('No Data Found', style: GoogleFonts.abel()),
                           ],
                         ),
                       );
@@ -141,23 +153,24 @@ Future<QuerySnapshot<Map<String, dynamic>>> getChatsForMonth(DateTime startOfMon
                     return ListView.builder(
                       itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, index) {
-                      var data = snapshot.data!.docs[index];
-                      var allData = data['messageText'];
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Card(
-                            elevation: 5,
-                            shadowColor: Colors.green,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text(allData,style: GoogleFonts.habibi(fontSize:16,fontWeight:FontWeight.bold),),
+                        var data = snapshot.data!.docs[index];
+                        var allData = data['messageText'];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Card(
+                              elevation: 5,
+                              shadowColor: Colors.green,
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(allData, style: GoogleFonts.habibi(fontSize: 16, fontWeight: FontWeight.bold)),
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 8,),
-                        ],
-                      );
-                    });
+                            SizedBox(height: 8),
+                          ],
+                        );
+                      },
+                    );
                   },
                 ),
               ),
